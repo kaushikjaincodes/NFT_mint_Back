22159374319d683da6f78b6903e87f3334b65524
kaushikjaincodes <kaushikjain2626@gmail.com>
2024-10-18 03:47:43 +0530

    Cors

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use((req, res, next) => {
+  res.header('A')
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
